81066862d0c0332b9da221f75d2176ed65bb2c95
Eileen McFarland and Olga Talovikova <pair+eileen+olga@allstate.com>
2016-06-27 11:27:42 -0500

    added Sum component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-import App from './App';
+import Sum from './Sum';
 import React from 'react';
 import { render } from 'react-dom';
 
-render(<App />, document.getElementById('root'));
+let tags = [];
+for (let i = 0; i < 10; i++) {
+  let rnd1 = Math.floor(Math.random() * 100);
+  let rnd2 = Math.floor(Math.random() * 100);
+  let tag = <Sum key={i} x={rnd1} y={rnd2} />;
+  tags.push(tag);
+}
+
+render(
+  <div>
+    {tags}
+  </div>, document.getElementById('root'));
